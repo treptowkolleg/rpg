326d6ffaa30e43fa9a716028f076fdcf03da2783
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <kv@treptow-kolleg.de>
2024-07-07 09:25:36 +0200

    add user input

--- src/Character/UserCharacter.php
+++ src/Character/UserCharacter.php
@@ -7,7 +7,7 @@ use Btinet\Rpg\Item\Potion;
 class UserCharacter extends AbstractCharacter
 {
 
-    private array $potions;
+    public array $potions;
 
     public function __construct(string $name, int $hp, int $ap, float $attackFactor, int $dp, float $defenseFactor)
     {
@@ -20,13 +20,15 @@ class UserCharacter extends AbstractCharacter
         parent::__construct($name, $hp, $ap, $attackFactor, $dp, $defenseFactor);
     }
 
-    public function heal(UserCharacter $p1)
+    public function heal(UserCharacter $character = null)
     {
+        if(!$character) $character = $this;
+
         if(count($this->potions) > 0) {
-            $usedPotion = array_shift($this->potions);
-            $this->addHp($usedPotion->getHp());
-            echo "\e[39m♡ $this um {$usedPotion->getHp()}HP geheilt.\n";
-            echo "\e[93m♡ $this hat nun {$this->getHp()}/{$this->getMaxHP()} HP!\n\n";
+            $usedPotion = array_shift($character->potions);
+            $character->addHp($usedPotion->getHp());
+            echo "\e[39m♡ $character um {$usedPotion->getHp()}HP geheilt.\n";
+            echo "\e[93m♡ $character hat nun {$character->getHp()}/{$character->getMaxHP()} HP!\n\n";
         } else {
             echo "\e[39mKeine Potions mehr verfügbar!\n";
         }
