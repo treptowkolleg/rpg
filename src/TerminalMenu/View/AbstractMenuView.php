43f1bd2aba74c88483c16469e3888df2246e0a53
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-08-17 09:47:50 +0200

    update super class

--- src/TerminalMenu/View/AbstractMenuView.php
+++ src/TerminalMenu/View/AbstractMenuView.php
@@ -5,7 +5,7 @@ namespace Btinet\Rpg\TerminalMenu\View;
 use Btinet\Rpg\Engine\SimpleTerminalEngine;
 use Btinet\Rpg\TerminalMenu\TerminalMenu;
 
-abstract class AbstractMenuView
+abstract class AbstractMenuView implements MenuViewInterface
 {
 
     protected TerminalMenu $menu;
@@ -15,6 +15,7 @@ abstract class AbstractMenuView
     public function __construct(SimpleTerminalEngine $engine)
     {
         $this->engine = $engine;
+        $this->setup();
     }
 
     public function getMenu(): TerminalMenu
